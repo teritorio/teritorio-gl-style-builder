9c07a218fb70f9aacbd32137fa72a34f7cf4f6e7
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-05-02 16:32:27 +0200

    Update colors

--- flat_ico.sh
+++ flat_ico.sh
@@ -2,27 +2,14 @@
 
 set -e
 
-declare -A colors
-colors['products']='#F25C05'
-colors['convenience']='#00a0a4'
-colors['services']='#2a62ac'
-colors['safety']='#e42224'
-colors['mobility']='#008ecf'
-colors['amenity']='#2a62ac'
-colors['remarkable']='#e50980'
-colors['culture']='#76009e'
-colors['hosting']='#99163a'
-colors['catering']='#F5B700'
-colors['leisure']='#00A757'
-colors['public_landmark']='#1D1D1B'
-colors['shopping']='#808080'
-default_color='rgba(255, 255, 255, 1)'
+INPUT=$1
+COLORS=$2
 
-
+source $COLORS
 
 rm -fr /tmp/ico_i && mkdir /tmp/ico_i
 rm -fr /tmp/ico && mkdir /tmp/ico
-cd icones:pictos:vectoriel
+cd $INPUT
 find . -name "*.svg" | while read ico; do
   echo "$ico"
   OUT=${ico#.\/}
@@ -45,7 +32,7 @@ find . -name "*.svg" | while read ico; do
 
   OUT_B=${OUT/.svg/•.svg}
   rsvg-convert --keep-aspect-ratio $RESIZE_11 -f svg "$ico" -o "/tmp/ico/$OUT_B"
-  sed -i 's/fill:[^;]*/fill:rgb(30%,30%,30%)/g' "/tmp/ico/$OUT_B"
+  sed -i 's/fill=\"[^\"]*\"/fill=\"rgb(30%,30%,30%)\"/g' "/tmp/ico/$OUT_B"
 
   C=${colors[$SUPER_CLASS]}
   C=${C:-$default_color}
@@ -56,10 +43,10 @@ find . -name "*.svg" | while read ico; do
   W=${WH%-*}
   H=${WH#*-}
 
-  sed -i "s/fill:[^;]*/fill:rgb(30%,30%,30%)/g" "/tmp/ico_i/$OUT_B"
+  sed -i "s/fill=\"[^\"]*\"/fill=\"rgb(30%,30%,30%)\"/g" "/tmp/ico_i/$OUT_B"
 
   OUT_W=${OUT/.svg/⬤.svg}
-  sed "s/fill:rgb(30%,30%,30%)/fill:#FFFFFF/g" < "/tmp/ico_i/$OUT_B" > "/tmp/ico_i/$OUT_W"
+  sed "s/fill=\"rgb(30%,30%,30%)\"/fill=\"#FFFFFF\"/g" < "/tmp/ico_i/$OUT_B" > "/tmp/ico_i/$OUT_W"
 
   X=$(((30-$W)/2))
   Y=$(((30-$H)/2))
