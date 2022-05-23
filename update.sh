2f0a216cca4d14eb553000b0a03a7b89ab9de7d7
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-05-23 18:11:12 +0200

    Add check on build

--- update.sh
+++ update.sh
@@ -6,11 +6,18 @@ STYLE_DIR=$1
 
 source $STYLE_DIR/.env
 
-curl https://vecto-dev.teritorio.xyz/data/teritorio-${THEME}-ontology-dev.json > teritorio-${THEME}-ontology.json
+curl https://vecto.teritorio.xyz/data/teritorio-${THEME}-ontology-latest.json > teritorio-${THEME}-ontology.json
 curl https://drive.teritorio.fr/index.php/s/FWntdw9fF3qnLW7/download > icones_pictos_vectoriel_flat.zip
 
 rm -fr 'icones:pictos:vectoriel_flat'
 unzip icones_pictos_vectoriel_flat.zip
+
+echo "Duplicates SVG content:"
+find icones:pictos:vectoriel_flat | xargs md5sum | sort | uniq -w 32 -D
+
+echo "Duplicate names in icons/extra"
+find icones:pictos:vectoriel_flat | cut -d '/' -f 3 | sort | uniq -D
+
 rm -fr "icones:pictos:vectoriel_flat/extra"
 
 rm -fr icons_tree
